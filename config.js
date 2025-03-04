350622ed535190e688a89f43e052abefb0cf7ef5
WassimTorjmen <torjmane521@gmail.com>
2025-03-04 10:51:13 +0100

    First commit

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  s3: {
+    accessKeyId: 'AKIASFIXCPWSHWICKCL2', // TODO
+    secretAccessKey: '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', // TODO
+    endpoint: 's3.eu-west-1.amazonaws.com', // TODO
+    bucket: 'esgi-cloud-exam-bucket-21052', // TODO
+  },
+  suffix: {
+    small: '_small.jpg',
+    full: '_full.jpg'
+  }
+};
